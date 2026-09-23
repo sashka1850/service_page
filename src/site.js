4e8d88e0423e989b7cce387f37b6e9ad53ad9621
sashka1850 <97841756+sashka1850@users.noreply.github.com>
2026-09-24 00:02:28 +0300

    Tune secondary text animation

--- src/site.js
+++ src/site.js
@@ -114,7 +114,7 @@ function typeText(element) {
       window.clearInterval(interval);
       element.classList.add('is-typed');
     }
-  }, 18);
+  }, 30);
 }
 
 function setupRevealAnimations() {
