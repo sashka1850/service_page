ac5bee368618b93ab446998856ff7a22e68d7bda
sashka1850 <97841756+sashka1850@users.noreply.github.com>
2026-09-25 00:52:38 +0300

    Fix site script syntax (src/site.js)

--- src/site.js
+++ src/site.js
@@ -70,7 +70,7 @@ brand.addEventListener('change', () => {
 model.addEventListener('change', updatePrice);
 $('#calculator-form').addEventListener('submit', event => {
   event.preventDefault();
-}));
+});
 const serviceCards = [...document.querySelectorAll('.service-card')];
 function closeServiceCard(card) {
   const button = card.querySelector('.service-toggle');
